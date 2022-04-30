0fafc440bc94f9a0c2e4e50211890b3d564d73d8
romaniulian <iulian.roman@protonmail.com>
2022-04-30 12:53:20 +0300

    Create homepage

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,7 +19,7 @@ describe('AppComponent', () => {
   it(`should have as title 'IdeaProjects'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('IdeaProjects');
+    expect(app.title).toEqual('Homepage');
   });
 
   it('should render title', () => {
